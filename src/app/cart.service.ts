887a56edba16341816ea3fff04f173aee5ca0d8d
TearoseIr <129524339+TearoseIr@users.noreply.github.com>
2023-08-26 10:49:04 +0500

    2 files added

---
+++ src/app/cart.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {Product} from './products';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  constructor() { }
+
+  items:Product[] = [];
+
+  addToCart(product:Product){
+    this.items.push(product);
+  }
+
+  getitems():Product[]{
+    return this.items;
+  }
+
+  clearCart():Product[]{
+    this.items=[]
+    return this.items;
+  }
+}
