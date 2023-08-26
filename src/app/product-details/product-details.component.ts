69dd2987771f670b57d5fe7501ce4234b2d81d53
TearoseIr <129524339+TearoseIr@users.noreply.github.com>
2023-08-26 14:18:46 +0500

    2 files modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Product, products} from '../products';
+import {CartService} from '../cart.service';
+
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
@@ -9,7 +11,8 @@ import {Product, products} from '../products';
 export class ProductDetailsComponent implements OnInit {
 
      product: Product | undefined;
-     constructor(private route: ActivatedRoute){}
+     constructor(private route: ActivatedRoute,
+      private cartService: CartService){}
 
   ngOnInit(){
     const routeParams = this.route.snapshot.paramMap;
@@ -17,8 +20,11 @@ export class ProductDetailsComponent implements OnInit {
     this.product = products.find(product => product.id === productIdFromRoute);
     
     console.log('получили инфу по товару ', this.product);
-    
+    }
 
-  }
+    addToCart(product:Product){
+      this.cartService.addToCart(product);
+      window.alert('Your product has been added to the cart!');
+    }
    
  }
